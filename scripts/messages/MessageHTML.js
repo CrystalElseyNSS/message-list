9d6d0ad3629bd2a4e378139ffd6755b82ab31836
Crystal Elsey <crystalelsey@Crystals-MacBook-Pro.local>
2020-03-10 10:51:12 -0500

    initial commit

---
+++ scripts/messages/MessageHTML.js
@@ -0,0 +1,12 @@
+export const Message = messageObject => {
+    return `
+        <section class="message">${messageObject.friend}: ${messageObject.text}</section>
+    `
+}
+
+export const Message = messageObject => {
+    return `
+        <section class="message">${messageObject.friend}: ${messageObject.text}</section>
+    `
+}
+
